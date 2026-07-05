858bcb350010a6df2d5b3ea286cb879b106e6aa8
summercat <rjbzd12345@gmail.com>
2026-07-05 11:10:49 +0800

    Remove user message avatar

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1866,9 +1866,7 @@ function _Chat() {
                                   }}
                                 ></IconButton>
                               </div>
-                              {isUser ? (
-                                <Avatar avatar={config.avatar} />
-                              ) : (
+                              {!isUser && (
                                 <>
                                   {["system"].includes(message.role) ? (
                                     <Avatar avatar="2699-fe0f" />
